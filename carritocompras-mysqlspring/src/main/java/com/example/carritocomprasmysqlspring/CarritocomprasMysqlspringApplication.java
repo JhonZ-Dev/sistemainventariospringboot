97717ebe468b5c1163b600375112adf71154e17e
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-17 21:29:45 -0500

    Create Proyect

---
+++ carritocompras-mysqlspring/src/main/java/com/example/carritocomprasmysqlspring/CarritocomprasMysqlspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.carritocomprasmysqlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritocomprasMysqlspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritocomprasMysqlspringApplication.class, args);
+	}
+
+}
---
+++ carritocompras-mysqlspring/src/test/java/com/example/carritocomprasmysqlspring/CarritocomprasMysqlspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carritocomprasmysqlspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritocomprasMysqlspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
