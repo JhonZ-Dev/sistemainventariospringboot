ea9c89ca24aca78ff7d26bdd64fba5bae3558545
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-17 21:40:49 -0500

    Update Usuarios.java

--- carritocompras-mysqlspring/src/main/java/com/example/carritocomprasmysqlspring/modelos/Usuarios.java
+++ carritocompras-mysqlspring/src/main/java/com/example/carritocomprasmysqlspring/modelos/Usuarios.java
@@ -1,4 +1,20 @@
 package com.example.carritocomprasmysqlspring.modelos;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Data
+@Slf4j
 public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_usuario;
+    private String nombres;
+    private String email;
+    private String password_user;
+    private String id_rol;
 }
