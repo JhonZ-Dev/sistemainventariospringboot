f18297988500c76796617ac67fc30bfa6f1dfb4a
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-19 21:46:16 -0500

    Update Roles.java

--- carritocompras-mysqlspring/src/main/java/com/example/carritocomprasmysqlspring/modelos/Roles.java
+++ carritocompras-mysqlspring/src/main/java/com/example/carritocomprasmysqlspring/modelos/Roles.java
@@ -1,9 +1,13 @@
 package com.example.carritocomprasmysqlspring.modelos;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_roles")
 public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_rol;
+    
 }
